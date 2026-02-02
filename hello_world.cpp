dd48427d643baba45c7d8593e8f89547e2c1dc73
Kami <josephkiarie561@gmail.com>
2026-02-02 20:45:38 +0300

    notes.md

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 int main(){
-    std::cout << "I like Pasta!";
+    std::cout << "I like Pasta!" << "\n";
+    std::cout << "Its really good" << "\n";
     return 0;
 }
---
+++ notes.md
@@ -0,0 +1,10 @@
+# CPP Notes
+## basic hellow world code:
+```C++
+#include <iostream>
+ int main(){
+    // this is a comment
+
+    std::cout << "Hello, World!" << "\n";
+    return 0;
+ }
