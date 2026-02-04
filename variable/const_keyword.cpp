54ecd9c7364f4b079b2f0607623bfd57ed47f465
Kami <josephkiarie561@gmail.com>
2026-02-04 21:15:27 +0300

    const keyword

--- variable/const_keyword.cpp
+++ variable/const_keyword.cpp
@@ -4,9 +4,9 @@
 #include <iostream>
 int main() {
     // this program calculates the circumfrence
-    const double pi = 3.14159;
+    const double PI = 3.14159;
     double radius = 10;
-    double circumference = 2 * pi * radius;
+    double circumference = 2 * PI * radius;
 
     std::cout << circumference << "cm";
 
