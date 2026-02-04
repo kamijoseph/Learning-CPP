c5269b7c96e5f5abd2a260b509a634a7227e9a95
Kami <josephkiarie561@gmail.com>
2026-02-04 20:58:14 +0300

    variables_practice

---
+++ variable/variables_practice.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+int main() {
+
+    // defning a string
+    std::string name = "Kami";
+    std::string friend_name = "Morine";
+
+    // defining integer
+    int age = 24;
+    int morine_age = 22;
+
+    std::cout << "My name is " << name << " and i am " << age << "years old" << "\n";
+    std::cout << "My friend's name is " << friend_name << " and she is " << morine_age << "\n";
+
+    return 0;
+}
