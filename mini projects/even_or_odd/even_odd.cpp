76c3d4ce460537b3ee31b35ea902e8996d856bbc
Kami <josephkiarie561@gmail.com>
2026-02-06 20:53:03 +0300

    even odd  init

---
+++ mini projects/even_or_odd/even_odd.cpp
@@ -0,0 +1,21 @@
+
+// check number if even or odd
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+
+    // first number
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    // second number
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+
+
+    return 0;
+}
