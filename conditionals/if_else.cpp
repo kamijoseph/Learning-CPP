17a54d3bd36469f7bb1256b5d28aba5a46cf8800
Kami <josephkiarie561@gmail.com>
2026-02-13 22:09:59 +0300

    jknvfskjvnfjkbv

--- conditionals/if_else.cpp
+++ conditionals/if_else.cpp
@@ -2,7 +2,16 @@
 #include <iostream>
 int main(){
 
-    std::cout << "conditionals\n";
+    int age;
+
+    std::cout << "enter your age: ";
+    std::cin >> age;
+
+    if (age >= 18){
+        std::cout << "you are an adult\n";
+    } else {
+        std::cout << "you child champ. logout\n";
+    }
 
     return 0;
 }
