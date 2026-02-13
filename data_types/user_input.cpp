f9673d1d87a62fbcc37f8ca3ecd3672eff63a901
Kami <josephkiarie561@gmail.com>
2026-02-13 20:48:53 +0300

    getline function whitespace ws func

--- data_types/user_input.cpp
+++ data_types/user_input.cpp
@@ -3,15 +3,15 @@
 
 int main(){
 
-    // // unspaced input (string)
-    // std::string name;
-    // std::cout << "what is your name?: ";
-    // std::cin >> name;
+    // unspaced input (string)
+    std::string name;
+    std::cout << "what is your name?: ";
+    std::cin >> name;
 
-    // using getline(cin, var) function for spaced strings
+    // using getline(std::cin >> std::ws, var) function for spaced strings: ws - whitespaces
     std::string full_name;
     std::cout << "what is your full name: ";
-    std::getline(std::cin, full_name);
+    std::getline(std::cin >> std::ws, full_name);
 
     std::cout << "hello " << full_name << "\n";
 
