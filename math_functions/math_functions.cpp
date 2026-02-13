26cda017b00c7ec348a0c9884c7f9f0bea050975
Kami <josephkiarie561@gmail.com>
2026-02-13 21:26:14 +0300

    a few useful math functions from cmath

--- math_functions/math_functions.cpp
+++ math_functions/math_functions.cpp
@@ -1,9 +1,10 @@
 
 #include <iostream>
+#include <cmath>
 
 int main(){
 
-    double x = 3;
+    double x = 3.14;
     double y = 4;
 
     // max function: returns the greater
@@ -13,6 +14,22 @@ int main(){
     // min function: returns smallest
     double minNum = std::min(x, y);
     std::cout << "smallest value is " << minNum << "\n";
+
+    // pow function
+    double power = pow(2, 5);
+    std::cout << "power is " << power << "\n";
+
+    // square root
+    double squareRoot = sqrt(y);
+    std::cout << "the square root of " << y << " is " << squareRoot << "\n";
+
+    // rounding down
+    double roundedDown = round(x);
+    std::cout << "rounded down is " << roundedDown << "\n";
+
+    // rounding up
+    double roundedUp = ceil(x);
+    std::cout << "rounded up is " << roundedUp << "\n";
 
     return 0;
 }
