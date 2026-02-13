85e0d455aa8d50b7bb9d96f23f99d05ffe01eeb4
Kami <josephkiarie561@gmail.com>
2026-02-13 22:21:46 +0300

    too old champ, rest

--- conditionals/if_else.cpp
+++ conditionals/if_else.cpp
@@ -9,6 +9,12 @@ int main(){
 
     if (age >= 18){
         std::cout << "you are an adult\n";
+    } else if (age < 0) {
+        std::cout << "you havent even been born yet pal\n";
+    } else if (age == 0){
+        std::cout << "you were just born\n";
+    } else if (age >= 100){
+        std::cout << "too old mate. rest now\n";
     } else {
         std::cout << "you child champ. logout\n";
     }
