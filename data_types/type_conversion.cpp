52dcae93eeb8dceb67f6652cedb729c21d690487
Kami <josephkiarie561@gmail.com>
2026-02-13 20:30:47 +0300

    type conversion example

--- data_types/type_conversion.cpp
+++ data_types/type_conversion.cpp
@@ -2,6 +2,13 @@
 #include <iostream>
 int main(){
 
+    // type conversion:
+
+    int correct = 8;
+    int total = 10;
+    double score = (double) correct / (double) total * 100;
+
+    std::cout << score << "%\n";
 
     return 0;
 }
