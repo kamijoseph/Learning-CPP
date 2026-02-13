f46f9d555a45c8e37323f54ba3ad965275a829d0
Kami <josephkiarie561@gmail.com>
2026-02-13 21:46:22 +0300

    hypotenuse submission

--- mini projects/hypotenuse/calculate_hypotenuse.cpp
+++ mini projects/hypotenuse/calculate_hypotenuse.cpp
@@ -4,7 +4,17 @@
 
 int main(){
 
-    std::cout << "calculating hypotenuse .....\n";
+    double a, b, c;
+    
+    std::cout << "enter length of the triangle: ";
+    std::cin >> a;
+
+    std::cout << "enter the height of the triangle: ";
+    std::cin >> b;
+
+    c = sqrt(pow(a, 2) + pow(a, 2));
+    std::cout << "the power of a triangle with height " << b << " and length " << a << " is " << c << "\n";
+
 
     return 0;
 }
