16945c13bee026ed66390d45ba401a35a00efec6
Kami <josephkiarie561@gmail.com>
2026-02-13 20:39:18 +0300

    user input

---
+++ data_types/user_input.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main(){
+    std::string name;
+    std::cout << "what is your name?: ";
+    std::cin >> name;
+
+    std::cout << "hello " << name << "\n";
+
+    return 0;
+}
