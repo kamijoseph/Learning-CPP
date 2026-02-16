c27398a3987b37bf8433798c0b8bc6264c24cee6
Kami <josephkiarie561@gmail.com>
2026-02-16 21:03:55 +0300

    degree celcious to degrees fahrenheit

--- mini projects/temperature converter/temperature_converter.cpp
+++ mini projects/temperature converter/temperature_converter.cpp
@@ -4,15 +4,23 @@
 int main(){
 
     double temp;
-    char unit;
+    std::string unit;
 
-    std::cout << "****** tmperature converter *****";
+    std::cout << "****** tmperature converter *****\n";
     std::cout << "F = fahrenheit\n";
     std::cout << "C = celsius\n";
     std::cout << "what unit would you like to convert to: ";
     std::cin >> unit;
 
+    if (unit == "F" || unit == "f"){
+        std::cout << "enter the temperature in celcius: ";
+        std::cin >> temp;
 
+        temp = (1.8 * temp) + 32.0;
+        std::cout << "temperature is: " << temp << " fahrenheit\n\n";
+    } else {
+        std::cout << "error\n";
+    }
 
     std::cout << "***************************************";
 
