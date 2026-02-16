0cb5684682a0a19f5fc25fc28f6777768bf528cc
Kami <josephkiarie561@gmail.com>
2026-02-16 20:55:09 +0300

    temperature converter program inititalization in c++

---
+++ mini projects/temperature converter/temperature_converter.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+int main(){
+
+    double temp;
+    char unit;
+
+    std::cout << "****** tmperature converter *****";
+    std::cout << "F = fahrenheit\n";
+    std::cout << "C = celsius\n";
+    std::cout << "what unit would you like to convert to: ";
+    std::cin >> unit;
+
+
+
+    std::cout << "***************************************";
+
+
+    return 0;
+}
