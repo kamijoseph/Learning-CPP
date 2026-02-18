5ebfcddd8627d5252fde4d2d231861f62772c69e
Kami <josephkiarie561@gmail.com>
2026-02-18 21:33:32 +0300

    yaaaay, descending

--- loops/for loops/for_loops.cpp
+++ loops/for loops/for_loops.cpp
@@ -4,9 +4,17 @@
 
 int main(){
 
-    for (int idx = 1; idx <= 3; idx++){
-        std::cout << "happy new year\n";
+    //  loop to 10
+    for (int idx = 1; idx <= 10; idx++){
+        std::cout << idx << "\n";
     }
+    std::cout << "happy new year\n\n";
+
+    // descending order
+    for (int i = 10; i >= 0; i--){
+        std::cout << i << "\n\n";
+    }
+    std::cout << "succesfully counted 10 descending\n";
 
     return 0;
 }
