61a0b1e723aecd437e2f7d5c26cdb280c0f06801
Kami <josephkiarie561@gmail.com>
2026-02-18 22:41:28 +0300

    die throw rands

--- mini projects/random number/random_numbers.cpp
+++ mini projects/random number/random_numbers.cpp
@@ -2,9 +2,23 @@
 // generating pseudo random numbers
 
 #include <iostream>
+#include <ctime>
 int main() {
 
-    std::cout << "why pseudo random numbers? no randomness  in cs\n";
+    srand(time(NULL));
+
+    // we add a modulous so the number generated can be within our bounds in this case 100 rounds
+    // they start from zero hence the plus 
+
+    // lets roll 3 dices
+    int die1 = (rand() % 6) + 1;
+    int die2 = (rand() % 6) + 1;
+    int die3 = (rand() % 6) + 1;
+
+    std::cout << "dice 1 : " << die1 << " \n";
+    std::cout << "dice 2 : " << die2 << " \n";
+    std::cout << "dice 3 : " << die3 << " \n";
+
 
     return 0;
 }
