319c50e7ccc62fe0585ef2007524c31c4e5b1935
Kami <josephkiarie561@gmail.com>
2026-02-18 22:12:58 +0300

    user inputs for the shape drawer

--- loops/for loops/nested_loops.cpp
+++ loops/for loops/nested_loops.cpp
@@ -1,9 +1,20 @@
 
 // nested loops - loops inside other loops
+// creating a rectangle of symbols
 
 #include <iostream>
 
 int main(){
+
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "how many rows do you want?: ";
+    std::cin >> rows;
+
+    std::cout << "how many columns do you want?: ";
+    std::cin >> columns;
 
     for(int i = 1; i <= 3; i++){
         for (int j = 1; j <= 10; j++){
