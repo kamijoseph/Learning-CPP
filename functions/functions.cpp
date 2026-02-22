16c323b8bbf9290550926df127da2d8776470321
Kami <josephkiarie561@gmail.com>
2026-02-22 14:34:16 +0300

    more parameters/ arguments

--- functions/functions.cpp
+++ functions/functions.cpp
@@ -2,21 +2,27 @@
 #include <iostream>
 
 // creating a happyBirthday function
-void happyBirthday(std::string name){
-    std::cout << "happy birthday to " << name << "\n";
+void happyBirthday(std::string name, int age){
+    std::cout << "\nhappy birthday to " << name << "\n";
     std::cout << "happy birthday to " << name << "\n";
     std::cout << "happy birthday dear " << name << "\n";
     std::cout << "happy birthday to " << name << "\n";
+    std::cout << "you are now " << age << " years old\n\n";
 }
 
 int main(){
     
     // calling the happy birthday function
     std::string name;
+    int age;
+
     std::cout << "enter your name: ";
     std::cin >> name;
 
-    happyBirthday(name);
+    std::cout << "how old are you?: ";
+    std::cin >> age;
+
+    happyBirthday(name, age);
 
     return 0;
 }
