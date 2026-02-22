634b4f91c973bdf3ddd344102bcd1539523ee6ea
Kami <josephkiarie561@gmail.com>
2026-02-22 14:53:56 +0300

    volume function

--- functions/return_functions.cpp
+++ functions/return_functions.cpp
@@ -6,13 +6,16 @@
 
 // function declaration
 double square(double length);
+double cubed(double length);
 
 int main(){
 
     double length = 5.0;
     double area = square(length);
+    double volume = cubed(length);
 
     std::cout << "area: " << area << "cm^2\n";
+    std::cout << "volume: " << volume << "cm^3\n";
 
     return 0;
 }
@@ -20,6 +23,11 @@ int main(){
 // square function
 double square(double length){
     return length * length;
+}
+
+// volume of a cube
+double cubed(double length){
+    return length * length * length;
 }
 
 // notice the function is no longer void but double
