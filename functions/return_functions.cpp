fb010c9b8c787073b79cc359deb254dca9b070c4
Kami <josephkiarie561@gmail.com>
2026-02-22 14:49:31 +0300

    calling a function with its thingie

--- functions/return_functions.cpp
+++ functions/return_functions.cpp
@@ -4,9 +4,23 @@
 
 #include <iostream>
 
+// function declaration
+double square(double length);
+
 int main(){
 
-    std::cout << "functions with a return keywords\n";
+    double length = 5.0;
+    double area = square(length);
+
+    std::cout << "area: " << area << "cm^2\n";
 
     return 0;
 }
+
+// square function
+double square(double length){
+    return length * length;
+}
+
+// notice the function is no longer void but double
+// this is to match the results being returned
