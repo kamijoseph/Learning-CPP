0c1f87b749e0bde34b32b57946c83d3e87865488
Kami <josephkiarie561@gmail.com>
2026-02-22 15:04:05 +0300

    std::cout function type

--- functions/return_functions.cpp
+++ functions/return_functions.cpp
@@ -4,9 +4,10 @@
 
 #include <iostream>
 
-// function declaration
+// functions declaration
 double square(double length);
 double cubed(double length);
+std::string concatStrings(std::string string1, std::string string2);
 
 int main(){
 
@@ -14,8 +15,13 @@ int main(){
     double area = square(length);
     double volume = cubed(length);
 
+    std::string firstName = "Kami";
+    std::string secondName = "Joseph";
+    std::string fullName = concatStrings(firstName, secondName);
+
     std::cout << "area: " << area << "cm^2\n";
     std::cout << "volume: " << volume << "cm^3\n";
+    std::cout << "your full name is: " << fullName << "\n";
 
     return 0;
 }
@@ -28,6 +34,11 @@ double square(double length){
 // volume of a cube
 double cubed(double length){
     return length * length * length;
+}
+
+// concatenate strings function
+std::string concatStrings(std::string string1, std::string string2){
+    return string1 + " " + string2;
 }
 
 // notice the function is no longer void but double
