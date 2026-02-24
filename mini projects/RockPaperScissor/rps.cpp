15c8a04d0a4bbd959d88cd5ac8a7f1d83c387e57
Kami <josephkiarie561@gmail.com>
2026-02-24 22:46:14 +0300

    rock papers scissors skeleton

--- mini projects/RockPaperScissor/rps.cpp
+++ mini projects/RockPaperScissor/rps.cpp
@@ -2,9 +2,38 @@
 // rock paper scissors game
 #include <iostream>
 
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
+
 int main(){
 
-    std::cout << "rock papers scissor game\n";
+    
 
     return 0;
+}
+
+// get users choice function
+char getUserChoice(){
+    
+    return 0;
+}
+
+// get computers choice function
+char getComputerChoice(){
+
+    return 0;
+}
+
+// show choice function
+void showChoice(char choice){
+
+    
+}
+
+// choose winner function
+void chooseWinner(char player, char computer){
+
+
 }
