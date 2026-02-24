987e3f6cca3adc9ee3249fc323046fb4a0c4dfe0
Kami <josephkiarie561@gmail.com>
2026-02-24 21:50:17 +0300

    main function's logic

--- mini projects/Banking/BankingProgram.cpp
+++ mini projects/Banking/BankingProgram.cpp
@@ -12,13 +12,55 @@ int main(){
     double balance = 0;
     int choice = 0;
 
-    std::cout << "**********************\n";
-    std::cout << "enter your choice:\n";
-    std::cout << "**********************\n";
-    std::cout << "1. show balance\n";
-    std::cout << "2. deposit money\n";
-    std::cout << "3. withdraw money\n";
-    std::cout << "4. exit\n";
+    
+
+    do{
+
+        std::cout << "**********************\n";
+        std::cout << "enter your choice:\n";
+        std::cout << "**********************\n";
+        std::cout << "1. show balance\n";
+        std::cout << "2. deposit money\n";
+        std::cout << "3. withdraw money\n";
+        std::cout << "4. exit\n";
+        std::cin >> choice;
+        
+        switch(choice){
+            case 1:
+                showBalance(balance);
+                break;
+            
+            case 2:
+                balance += deposit();
+                break;
+            
+            case 3:
+                balance -= withdraw(balance);
+                break;
+
+            case 4:
+                std::cout << "thanks for visiting!\n";
+                break;
+
+            default:
+                std::cout << "invalid choice\n";
+        }
+    }while(choice != 4);
 
     return 0;
+}
+
+// show balance function
+void showBalance(double balance){
+
+}
+
+// deposit function
+double deposit(){
+
+}
+
+// withdraw function
+double withdraw(double balance){
+    
 }
