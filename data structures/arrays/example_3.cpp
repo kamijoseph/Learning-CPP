5b2c2e69caf388a175a339a6a350ff9297dc8538
Kami <josephkiarie561@gmail.com>
2026-02-28 21:00:53 +0300

    foreach loop: eases traversal over an iterable dataset

---
+++ data structures/arrays/example_3.cpp
@@ -0,0 +1,12 @@
+
+// foreach loop iteration
+// loop that eases the traversal over an iterable data set
+
+#include <iostream>
+
+int main(){
+
+    std::cout << "for each loop. ease of traversal\n";
+
+    return 0;
+}
