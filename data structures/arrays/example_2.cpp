ff77d0afd1773cbfd08691d746590732c6c83737
Kami <josephkiarie561@gmail.com>
2026-02-28 20:53:40 +0300

    iterating through an array example 2

--- data structures/arrays/example_2.cpp
+++ data structures/arrays/example_2.cpp
@@ -11,11 +11,23 @@ int main(){
         "Spongebob", "Patrick", "Squidward", "Sandy", "Plankton"
     };
 
-    // iterating through the array
+    // iterating through the students array
     int studentLength = sizeof(students) / sizeof(std::string);
 
     for (int idx = 0; idx < studentLength; idx++){
-        std::cout << students[idx] << "\n";
+        std::cout << students[idx] << "\n\n";
+    }
+
+    // char grades array
+    char grades[] = {
+        'A', 'B', 'c', 'D', 'E', 'F'
+    };
+
+    // iterate through grades
+    int gradesLength = sizeof(grades) / sizeof(char);
+
+    for (int i = 0; i < gradesLength; i++){
+        std::cout << "Grade " << i + 1 << " is: " << grades[i] << "\n";
     }
 
     return 0;
