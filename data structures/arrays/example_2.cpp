27c2e118509bc337e7cbb177c92ea8107a2941ce
Kami <josephkiarie561@gmail.com>
2026-02-28 20:39:56 +0300

    iterating through an array

--- data structures/arrays/example_2.cpp
+++ data structures/arrays/example_2.cpp
@@ -7,10 +7,14 @@ int main(){
         5.00, 7.50, 9.99, 15.00
     };
 
-    std::cout << prices[0] << "\n";
-    std::cout << prices[1] << "\n";
-    std::cout << prices[2] << "\n";
-    std::cout << prices[3] << "\n";
+    std::string students[] = {
+        "Spongebob", "Patrick", "Squidward", "Sandy"
+    };
+
+    // iterating through the array
+    for (int idx = 0; idx < 4; idx++){
+        std::cout << students[idx] << "\n";
+    }
 
     return 0;
 }
