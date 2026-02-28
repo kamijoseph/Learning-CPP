49ee2d7a565d0194e95606ba4f576f9bb8eeaf91
Kami <josephkiarie561@gmail.com>
2026-02-28 11:51:01 +0300

    jkgbisaj

--- data structures/arrays/example_2.cpp
+++ data structures/arrays/example_2.cpp
@@ -3,7 +3,14 @@
 
 int main(){
 
-    std::cout << "example two\n";
+    double prices[] = {
+        5.00, 7.50, 9.99, 15.00
+    };
+
+    std::cout << prices[0] << "\n";
+    std::cout << prices[1] << "\n";
+    std::cout << prices[2] << "\n";
+    std::cout << prices[3] << "\n";
 
     return 0;
 }
