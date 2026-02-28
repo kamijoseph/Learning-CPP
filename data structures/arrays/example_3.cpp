ebeaa06155d8816d99faff4686b6814f74978602
Kami <josephkiarie561@gmail.com>
2026-02-28 21:06:49 +0300

    for each loop: eases the looping

--- data structures/arrays/example_3.cpp
+++ data structures/arrays/example_3.cpp
@@ -6,7 +6,14 @@
 
 int main(){
 
-    std::cout << "for each loop. ease of traversal\n";
+    std::string students[] = {
+        "Spongebob", "Patrick", "sandy", "Plankton", "Krabs"
+    };
+
+    // iterating with foreach looop - less flexible - starts only at beginning and to the end
+    for(std::string student : students){
+        std::cout << student << "\n";
+    }
 
     return 0;
 }
