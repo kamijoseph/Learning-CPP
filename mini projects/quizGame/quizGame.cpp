73733aa572276bf7d7516f8b755808418f0fa9ff
Kami <josephkiarie561@gmail.com>
2026-03-05 20:41:01 +0300

    zeeeem  arrays

--- mini projects/quizGame/quizGame.cpp
+++ mini projects/quizGame/quizGame.cpp
@@ -4,7 +4,25 @@
 
 int main(){
 
-    std::cout << "simple quize game mini project\n";
+    std::string questions[] = {
+        "1. what year was C++ created?: ",
+        "2. Who invented C++?: ",
+        "3. what is the predecessor of C++?: ",
+        "4. is the earth flat?: "
+    };
+
+    // options
+    std::string options[][4] = {
+        {"A. 1967", "B. 1990", "C. 1985", "D. 2002"},
+        {"A. Guido Van Rossum", "B. bjarne Stroustrup", "C. John Carmack", "D. Donald Trump"},
+        {"A. C", "B. C+", "C. C--", "D. Python"},
+        {"A. Yes", "B. No", "C. Sometimes", "D. Whats Earth?"}
+    };
+
+    // answer key array
+    char answerKey[] = {
+        'C', 'B', 'A', 'B'
+    };
 
     return 0;
 }
