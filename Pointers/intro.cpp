8d75ec59f601a0b3bad9c16aae0ada8f84701274
Kami <josephkiarie561@gmail.com>
2026-03-09 21:54:31 +0300

    pointers finally, yaaaaaaaaaay

---
+++ Pointers/intro.cpp
@@ -0,0 +1,12 @@
+
+// pointers - variables that stores a memory adress of another variable
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "pointers finally, yaaaaaaaaaaaaaay\n";
+
+    return 0;
+}
