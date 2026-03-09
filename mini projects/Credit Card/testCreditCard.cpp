6547b0534d6e3e49816056bf52a86aaa3a65a884
Kami <josephkiarie561@gmail.com>
2026-03-09 21:24:05 +0300

    zee functions

--- mini projects/Credit Card/testCreditCard.cpp
+++ mini projects/Credit Card/testCreditCard.cpp
@@ -2,9 +2,25 @@
 // test if a credit card number is valid: using luhns algorithm
 #include <iostream>
 
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNUmber);
+
 int main(){
 
     
 
+    return 0;
+}
+
+int getDigit(const int number){
+    return 0;
+}
+
+int sumOddDigits(const std::string cardNumber){
+    return 0;
+}
+
+int sumEvenDigits(const std::string cardNumber){
     return 0;
 }
